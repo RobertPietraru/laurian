0b89b25bb2aeaad3da35e24b3283b7f83d8ded1b
RobertPietraru <rob_piet@yahoo.com>
2024-10-08 18:57:36 +0300

    mesaj

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,6 +1,6 @@
 <main class="container mx-auto px-4 py-8 text-center h-[100vh] flex flex-col justify-center">
     <h1 class="text-4xl font-bold mb-4 text-purple-600">
-        Oops! Se pare că site-ul este în lucru!
+        Oops! Se pare că site-ul nu este disponibil momentan.
     </h1>
 
     <p class="text-xl mb-6 text-gray-700">
