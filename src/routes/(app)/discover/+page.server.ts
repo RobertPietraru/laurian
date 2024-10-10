db762efa18f93b3dde19e019f33eae92a3e137d0
RobertPietraru <rob_piet@yahoo.com>
2024-10-10 18:33:03 +0300

    changing project structure

--- src/routes/(app)/discover/+page.server.ts
+++ src/routes/(app)/discover/+page.server.ts
@@ -1,8 +1,12 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
 
         const clubs = await locals.clubRepository.getClubs(0, 50);
+        if (clubs === null) {
+            return error(500, 'Ceva nu a mers bine'); 
+        }
 
         return {
             clubs: clubs
